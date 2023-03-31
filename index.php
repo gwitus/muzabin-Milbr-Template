225fb194a42c776c7b7ab08ef414c1f659f8cdb4
taviobrmg <luizotavio.ti@hotmail.com>
2023-03-30 21:52:16 -0300

    Primeiro commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Milbr</title>
+    <link href="public/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="public/css/style.css">
+  </head>
+  <body>
+    <main>
+        <div class="container-fluid" id="carrousel">
+        </div>
+        <div class="container-fluid" id="vantagens">
+        </div>
+        <div class="container-fluid" id="precos">
+        </div>
+        <div class="container-fluid" id="localizacao">
+        </div>
+    </main>
+    <footer></footer>
+    <script src="public/js/bootstrap.bundle.min.js"></script>
+  </body>
+</html>
